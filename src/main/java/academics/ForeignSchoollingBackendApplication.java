20137210bb886c51df8489b781805bc22b10312c
DarlingtonOlelewe <o.darlington@outlook.com>
2022-08-03 08:04:04 +0100

    project setup

---
+++ src/main/java/academics/ForeignSchoollingBackendApplication.java
@@ -0,0 +1,13 @@
+package academics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForeignSchoollingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForeignSchoollingBackendApplication.class, args);
+	}
+
+}
